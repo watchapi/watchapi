bc30ebc603f9e5b12a7f6fa265ebd97572f2a321
ArseniyDev <jkl@usa.com>
2026-01-08 20:01:02 +0400

    fix: cli eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,19 @@
+const js = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+
+module.exports = [
+  {
+    ignores: ["dist/**", "out/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+];
