c6a3ca8fe290014881ac4f64523d25aa5d7d5305
ArseniyDev <jkl@usa.com>
2026-01-12 20:03:45 +0400

    docs: add docs to monorepo

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,15 @@
+import nextra from "nextra";
+
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+});
+
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+});
